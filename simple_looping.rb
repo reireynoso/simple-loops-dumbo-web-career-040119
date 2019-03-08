7ec84961cd5073bbe4ffcfd8cdc5bd4b916b5ae4
Reinald Reynoso <reireynoso@gmail.com>
2019-03-08 18:55:41 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     puts phrase
-    break if counter == number_of_times
     counter += 1
+    break if counter == number_of_times
   end  
 end
 
