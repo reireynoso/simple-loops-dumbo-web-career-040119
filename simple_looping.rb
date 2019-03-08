fa8f672e91eda64cd75f2ddc349ad6c6d2fb5abf
Reinald Reynoso <reireynoso@gmail.com>
2019-03-08 19:02:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -58,7 +58,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = number_of_times
   
-  for items in (0..counter).to_a
+  for items in 0..7
     puts phrase
   end
   
